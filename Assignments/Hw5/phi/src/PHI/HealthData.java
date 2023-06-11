0fd6ebedde025e4a229ccbb9a484f3534ed76c2a
mahimcbu <mahimc@bu.edu>
2023-06-11 11:34:32 -0400

    hw5 complete

---
+++ Assignments/Hw5/phi/src/PHI/HealthData.java
@@ -0,0 +1,25 @@
+package PHI;
+
+import java.util.Date;
+
+public abstract class HealthData<T> {
+    private String name;
+    private Date date;
+
+    public HealthData(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public abstract String getMetric();
+
+    public abstract T getData();
+}
---
+++ Assignments/Hw5/phi/src/PHI/HealthMetricsReader.java
@@ -0,0 +1,34 @@
+package PHI;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HealthMetricsReader {
+    /**
+     * Reads the ideal values for a given metric from the HealthMetrics file.
+     * 
+     * @param metric the metric for which to retrieve the ideal values
+     * @return an array containing the ideal values [lowerBound, upperBound]
+     * @precondition the HealthMetrics file exists and is accessible
+     * @postcondition the returned array contains the ideal values for the given metric,
+     *       or [0.0, 0.0] if the metric is not found in the file
+     */
+    public static double[] readIdealValue(String metric) {
+        double[] idealValues = new double[2];
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\mahim\\Documents\\GitHub\\CS622\\Assignments\\Hw3\\HealthMetrics.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                if (values.length >= 3 && values[0].trim().equals(metric.trim())) {
+                    idealValues[0] = Double.parseDouble(values[1].trim());
+                    idealValues[1] = Double.parseDouble(values[2].trim());
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the HealthMetrics file: " + e.getMessage());
+        }
+        return idealValues;
+    }
+}
+
